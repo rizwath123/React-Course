8fdb57f388ef2090b6e47bc7b4166e15aec1b4f8
rizwath123 <rizwathr@gmail.com>
2023-01-11 18:43:18 +0300

    login component created

---
+++ myfirstreactapp/src/components/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class LoginComp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>THIS IS LOGIN</h1>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                {/* <input type='text' placeholder="Enter Email">Email</input>
+                <input type='text' placeholder="Enter Password">Password</input> */}
+                {/* <button type="submit" style={ color: blue}> Submit</button> */}
+            </div>
+        )
+    }
+}
+
+export default LoginComp
